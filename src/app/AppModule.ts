a3591f7bdfaba99868f6e10679a7e662bdd9c7f6
Justin <justin@homeceuconnection.com>
2017-12-14 09:04:05 -0600

    Updating module

--- src/app/AppModule.ts
+++ src/app/AppModule.ts
@@ -1,4 +1,4 @@
-import {NgApiModule} from "./NgApiModule";
+import {NgApiModule} from "../lib/NgApiModule";
 import {Component, Injectable, NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {CommonModule} from "@angular/common";
